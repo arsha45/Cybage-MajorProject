4e4534085a42c9362143bbcac2a17856a4a9b6fd
Akanksha <akankshaz@cybage.com>
2024-02-11 23:13:17 +0530

    Modified components and Deleted unnecessay components

--- Frontend/Twitagram/src/app/post/post.module.ts
+++ Frontend/Twitagram/src/app/post/post.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './feed/feed.component';
-import { PostDetailComponent } from './post-detail/post-detail.component';
 import { CreateComponent } from './create/create.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { PostRoutingModule } from './post-routing/post-routing.module';
-import { CommentComponent } from './comment/comment.component';
+
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     FeedComponent,
-    PostDetailComponent,
     CreateComponent,
-    CommentComponent,
     
   ],
   imports: [
